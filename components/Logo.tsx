cef91eea1fa7a9c26812c2f2204bd00bb29c2ca1
gamsung2 <rocketb2025@gmail.com>
2026-05-13 13:27:57 +0900

    Replace logo image

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function Logo() {
   return (
     <Link href="/" className="logo" aria-label="은혜의빛교회 홈">
-      <Image src="/images/logo-transparent.svg" alt="은혜의빛교회" width={1220} height={350} priority />
+      <Image src="/images/logo-current-transparent.png" alt="은혜의빛교회" width={2629} height={712} priority />
     </Link>
   );
 }
